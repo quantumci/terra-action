93c2793892fa1032dc44b67b0f049fbab60addf9
pradipbabar <pradipbaba13@gmail.com>
2023-10-03 15:05:12 +0530

    test

--- Entrypoint.sh
+++ Entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -l
 cat $INPUT_TFVARS
-echo "$2" >> $GITHUB_OUTPUT
-echo "$3" >> $GITHUB_OUTPUT
-echo "$4" >> $GITHUB_OUTPUT
+echo "$INPUT_AWS-ACCESS-KEY-ID" >> $GITHUB_OUTPUT
+echo "$INPUT_AWS-SECRET-ACCESS-KEY" >> $GITHUB_OUTPUT
+echo "$INPUT_AWS-DEFAULT-REGION" >> $GITHUB_OUTPUT
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
 
